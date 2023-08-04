dfe8fd2c9b8aae7481e5cee2c7b9e7935de3ce1a
дмитрий журбенко <dmitrijzurbenko@MacBook-Air-dmitrij.local>
2023-08-04 17:33:23 +0400

    page layout login and registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { MainComponent } from './views/main/main.component';
+import { UserModule } from './views/user/user.module';
 
-const routes: Routes = [{ path: '', component: LayoutComponent, children: [{ path: '', component: MainComponent }] }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: MainComponent },
+      { path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule) },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
