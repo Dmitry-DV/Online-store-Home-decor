0da4504fa68f80834c93d06a7f6b2ca19bb5fe74
дмитрий журбенко <dmitrijzurbenko@MacBook-Air-dmitrij.local>
2023-09-15 13:07:02 +0400

    product request

--- src/app/views/product/detail/detail.component.ts
+++ src/app/views/product/detail/detail.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { ProductService } from 'src/app/shared/services/product.service';
+import { environment } from 'src/environments/environment.development';
 import { ProductType } from 'src/types/product.type';
 
 @Component({
@@ -10,6 +12,8 @@ import { ProductType } from 'src/types/product.type';
 })
 export class DetailComponent {
   recomendedProducts: ProductType[] = [];
+  product!: ProductType;
+  serverStaticPath = environment.serverStaticPath;
 
   customOptions: OwlOptions = {
     loop: true,
@@ -37,9 +41,17 @@ export class DetailComponent {
     nav: false,
   };
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.productService.getProduct(params['url']).subscribe({
+        next: (data: ProductType) => {
+          this.product = data;
+        },
+      });
+    });
+
     this.productService.getBestProducts().subscribe({
       next: (data: ProductType[]) => {
         this.recomendedProducts = data;
